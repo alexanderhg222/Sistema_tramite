4326a34bd9a804a9c1404ec04edcd9eb33ad0807
Alexander HG <alexanderhuamanguevara@gmail.com>
2023-03-22 10:19:40 -0500

    4

--- Model/conexionbd.php
+++ Model/conexionbd.php
@@ -34,6 +34,19 @@ class conexionBD{
     }
 }
 
+$host = 'containers-us-west-115.railway.app';
+            $port = '6217';
+            $db   = 'railway';
+            $user = 'root';
+            $pass = 'laLJSxvE37agsFrEGDto';
+            $charset = 'utf8mb4';
+
+            $dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port";
+            try {
+                $pdo = new PDO($dsn, $user, $pass);
+            } catch (PDOException $e) {
+                throw new DOException($e->getMessage(), (int)$e->getCode());
+            }
 
 /* Conectar a una base de datos de MySQL invocando al controlador 
 ./$dsn = 'mysql:dbname=railway;host=containers-us-west-37.railway.app';
